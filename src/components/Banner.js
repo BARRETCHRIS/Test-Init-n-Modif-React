2586802cb686b89ce0ace91e6cc83c47481ecb4a
BARRETCHRIS <christelle.barret.poirot@gmail.com>
2024-07-07 10:59:01 +0200

    test start project

---
+++ src/components/Banner.js
@@ -0,0 +1,12 @@
+import '../styles/Banner.css'
+
+function Banner() {
+	const title = 'La maison jungle'
+	return  <div className="lmj-banner">
+                <h1>{title}</h1>
+            </div>
+                
+            
+}
+
+export default Banner
